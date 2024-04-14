1025380fe578c47c7f1b40501da53706480f1858
Santiago Restrepo <90574038+SantiagoGameDev@users.noreply.github.com>
2024-04-14 19:52:20 -0400

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,55 @@
 import React from "react";
 import "./home.css";
+import Santiago from "../../assets/avatar-1.svg";
 
 const Home = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Web Developer", "Web Designer", "UI/UX Designer" ];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
   return (
     <section className="home container" id="home">
       <div className="intro">
-        <img src="" alt="" className="home__img" />
+        <img src={Santiago} alt="" className="home__img" />
+         <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Santiago`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Game Developer", "Programmer", "Technical Artist", "Web Developer" ]'><span className="wrap">{text}</span></span></h1>
       </div>
     </section>
   );
