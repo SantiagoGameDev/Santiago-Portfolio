fbf1375db3b31d124bf0eb85b293a380217e1362
Santiago Restrepo <90574038+SantiagoGameDev@users.noreply.github.com>
2024-04-14 19:07:47 -0400

    first commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <section className="home container" id="home">
+      <div className="intro">
+        <img src="" alt="" className="home__img" />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/components/interests/Interests.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./interests.css";
+
+const Interests = () => {
+  return (
+    <div>Interests</div>
+  )
+}
+
+export default Interests
